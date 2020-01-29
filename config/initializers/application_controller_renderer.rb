0d64ca2c75b70dc3229bfaaf9f7aeffcc71d8f4b
tama <yuki023201025@gmail.com>
2020-01-29 16:37:55 +0900

    コメント、いいね、プロフィール編集機能の作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad6c0a386b6c86d108a8129054bc018f6a00d20e73cedea9f8ce160b8da029cf3c5b14a874cfc2558e99ba184894d4f0e8fefb8cfc558f7859de02cbb58b4bb1'
